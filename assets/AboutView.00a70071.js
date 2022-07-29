b4fd770cb610ce464073755c6f90018bd941b050
Daniel Suescun <profesionalsistemas14@pharmasan.co>
2022-07-29 18:28:29 -0500

    Commit gh-pages

---
+++ assets/AboutView.00a70071.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t}from"./index.eee8e4a7.js";const c={};function n(o,s){return a(),t("span",null,"Esta es la pagina de about")}const _=e(c,[["render",n]]);export{_ as default};
